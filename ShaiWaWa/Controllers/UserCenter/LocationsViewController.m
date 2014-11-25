2ce50aa35b171f4d82c71f8dc6b5ae618abf8cce
Carl <787887347@qq.com>
2014-11-25 13:33:27 +0800

    添加了侧滑

--- ShaiWaWa/Controllers/UserCenter/LocationsViewController.m
+++ ShaiWaWa/Controllers/UserCenter/LocationsViewController.m
@@ -10,10 +10,9 @@
 #import "UIViewController+BarItemAdapt.h"
 #import "UIView+CutLayer.h"
 #import "SVProgressHUD.h"
-#import "CSqlite.h"
 @interface LocationsViewController ()
 {
-    CSqlite *m_sqlite;
+    
 }
 @property (nonatomic,strong) CLLocationManager * locationManager;
 @property (nonatomic,strong) NSMutableArray * placemarks;
@@ -27,8 +26,6 @@
     if (self) {
         // Custom initialization
         _placemarks = [@[] mutableCopy];
-        m_sqlite = [[CSqlite alloc]init];
-        [m_sqlite openSqlite];
     }
     return self;
 }
@@ -175,29 +172,6 @@
 }
 
 
--(CLLocationCoordinate2D)zzTransGPS:(CLLocationCoordinate2D)yGps
-{
-    int TenLat=0;
-    int TenLog=0;
-    TenLat = (int)(yGps.latitude*10);
-    TenLog = (int)(yGps.longitude*10);
-    NSString *sql = [[NSString alloc]initWithFormat:@"select offLat,offLog from gpsT where lat=%d and log = %d",TenLat,TenLog];
-    //NSLog(sql);
-    sqlite3_stmt* stmtL = [m_sqlite NSRunSql:sql];
-    int offLat=0;
-    int offLog=0;
-    while (sqlite3_step(stmtL)==SQLITE_ROW)
-    {
-        offLat = sqlite3_column_int(stmtL, 0);
-        offLog = sqlite3_column_int(stmtL, 1);
-        
-    }
-    
-    yGps.latitude = yGps.latitude+offLat*0.0001;
-    yGps.longitude = yGps.longitude + offLog*0.0001;
-    return yGps;
-    
-}
 
 #pragma mark - UITextFieldDelegate
 - (BOOL)textFieldShouldReturn:(UITextField *)textField
